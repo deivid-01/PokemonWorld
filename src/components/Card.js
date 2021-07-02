c1bea4c788e56a8bdf0b660e225a1a987e5a6473
Deivid <datcx@hotmail.com>
2021-07-01 20:40:50 -0500

    Styling Home component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,25 @@
 import React from 'react'
 
+import './cards.css'
 
 function Card ({title,description,image})
 {
     console.log(title)
     console.log(description)
     return (
-        <div className='card text-center bg-red'>
-            <img src={image} alt=""/>
+        <div className='card text-center bg-red animate__animated animate__fadeInUp '>
+            
+            <div className="overflow">
+            <img src={image} alt="" className="card-img-top"/>
+            </div>
+
             <div className='card-body'>
+
+            <a href="#!" className="btn">
                 <h4 className='card-title'>{title}</h4>
+                </a>
                 <p className='card-text text-secondary'>{description}</p>
+                
             </div>
 
         </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'animate.css/animate.min.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
