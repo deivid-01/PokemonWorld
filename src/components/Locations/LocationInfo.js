79f2fe022e30bb39bc22ac7a76890d83b9a0ee6f
Deivid <datcx@hotmail.com>
2021-07-04 19:25:31 -0500

    Fixing : Location Card

--- src/components/Locations/LocationInfo.js
+++ src/components/Locations/LocationInfo.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
+import Typography from '@material-ui/core/Typography';
+import MapIcon from '@material-ui/icons/Map';
+import Chip from '@material-ui/core/Chip';
+import ZoomOutMapIcon from '@material-ui/icons/ZoomOutMap';
+import TimelineIcon from '@material-ui/icons/Timeline';
 const useStyles = makeStyles({
   root: {
+    alignContent:'center',
+    justifyContent:'center',
     minWidth: 275,
     position:'absolute',
+    background:'dark',
     left:'50%',
     top:'50%',
     WebkitTransform: 'translate(-50%, -50%)',
     transform: 'translate(-50%, -50%)'
+  },
+  header:{
+    textAlign:'center'
   },
   bullet: {
     display: 'inline-block',
@@ -39,7 +47,7 @@ function LocationInfo({location}) {
 
   return (
     <Card className={classes.root} variant="outlined">
-      <CardHeader
+      <CardHeader className={classes.header}
         title={location.names[1].name}
         subheader={"#"+location.id}
       />
@@ -47,27 +55,28 @@ function LocationInfo({location}) {
         <Typography className={classes.title} color="textSecondary" gutterBottom>
           Region:
         </Typography>
-        <Typography variant="h5" component="h2">
-          {location.region.name}
-        </Typography>
+        <Chip icon={<MapIcon/>}
+          label = {location.region.name[0].toUpperCase()+location.region.name.substring(1,location.region.name.length)} 
+         color={"primary"} />
         <Typography className={classes.pos} color="textSecondary">
           Areas:
         </Typography>
           {
             location.areas.map((area)=>{
               return (
-              <Typography variant="body2" component="p">
-              {area.name}
-            </Typography>
+                <Chip icon={<ZoomOutMapIcon/>}
+                label = {area.name[0].toUpperCase()+area.name.substring(1,area.name.length)} 
+               color={"secondary"} />
               )
             })
           }
           <Typography className={classes.pos} color="textSecondary">
           Generation:
         </Typography>
-        <Typography variant="body2" component="p">
-             {settleGeneration(location.game_indices[0].generation.name)}
-            </Typography>
+        <Chip icon={<TimelineIcon/>}
+          label = {settleGeneration(location.game_indices[0].generation.name)} 
+         color={"default"} />
+       
 
         
       </CardContent>
